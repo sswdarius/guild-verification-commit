3a44afb19305c982c53c74fb3a90a205ea5a2238
dsad <65560494+sswdarius@users.noreply.github.com>
2026-03-05 20:39:08 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:39:02.075Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:39:06.887Z
+Commit: Fix memory leak in event handlers

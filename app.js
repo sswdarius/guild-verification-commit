a8d9aa1aa639fbaa3772992e5a5ccb0f770d2ca5
dsad <65560494+sswdarius@users.noreply.github.com>
2026-03-05 20:38:55 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:38:40.516Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T17:38:53.818Z
+Commit: Fix broken links in documentation

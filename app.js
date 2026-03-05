4e0f97c797ec4f6fd4595b60aa777f3cda912a8b
dsad <65560494+sswdarius@users.noreply.github.com>
2026-03-05 20:38:41 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:38:35.779Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T17:38:40.516Z
+Commit: Resolve null pointer exception in user service
